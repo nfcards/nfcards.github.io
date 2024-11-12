c0b14da8ff31633eb2c988708d2bea9842e65baf
tyroPatel <tyresecook25@yahoo.com>
2024-11-12 14:53:58 -0600

    Visitors can change the color of #dynamicCard svg

---
+++ index.js
@@ -0,0 +1,15 @@
+function setCardColor(event, primary, secondary) {
+	if (!event.classList.contains("selected")) {
+		let cardBkg = document.querySelector("#card_bkg");
+		let logoColor = document.querySelector("#logo_color");
+
+		logoColor.style.fill = secondary;
+		cardBkg.style.fill = primary;
+		
+		document.querySelector("button.selected").classList.remove("selected");
+		event.classList.add("selected"); 
+	}
+
+	
+	
+}
